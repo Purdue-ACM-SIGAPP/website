e8f52ea55c9f6f966773978e5659f50cb39e0c34
Jordan <jordan@jordanpitlor.com>
2017-01-19 20:50:00 -0500

    create tab components

---
+++ components/Apps.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (
+    <h1>Our Apps!</h1>
+);
--- components/index.js
+++ components/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import About from './About';
+import Apps from './Apps';
+import ContactUs from './Contact';
 
-export default class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello world!</h1>
-        );
-    }
-}
+export default () => (
+    <div>
+        <AppBar title="ACM SigApp"/>
+        <Tabs>
+            <Tab label="About Us"><About/></Tab>
+            <Tab label="Our Apps"><Apps/></Tab>
+            <Tab label="Contact Us"><ContactUs/></Tab>
+        </Tabs>
+    </div>
+);
