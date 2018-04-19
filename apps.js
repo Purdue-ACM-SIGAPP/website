e3db72963efcb862094e4b8526eda20bcc6d833a
Jeff Hughes <jeff@jhughes.xyz>
2018-04-19 19:28:59 -0400

    Add BoilerFaves and new Logo

--- apps.js
+++ apps.js
@@ -17,5 +17,16 @@ module.exports = [
 		"Sports Center at Purdue University.",
                 picture: require("./img/apps/corec.png"),
                 playStoreLink: "https://play.google.com/store/apps/details?id=club.sigapp.purduecorecmonitor"
-	}
+	},
+	{
+                name: "BoilerFaves",
+                description: "BoilerFaves lets you know when your favorite foods are available at" + 
+		"Purdue dining courts. With BoilerFaves, you can see" +
+		"which dining courts foods are available at, get" +
+		"notifications when foods you like are available," + 
+		"set notification times for each meal, and discover" + 
+		"new foods at the dining courts",
+                picture: require("./img/apps/boilerfaves.png"),
+                playStoreLink: "https://play.google.com/store/apps/details?id=com.lshan.boilerfaves"
+        }
 ];
